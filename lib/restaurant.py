58fa3d54f51ab74a8ac7a26c552f72101dd5b31b
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-27 00:02:31 +0300

    Completed codechallenge

---
+++ lib/restaurant.py
@@ -0,0 +1,15 @@
+class Restaurant:
+    def __init__(self,name=None):
+        self._name = None
+        if name is not None:
+            self.set_name(name)
+        
+    def set_name (self,name):
+        if isinstance (name,str):
+            self._name = name
+            
+    def name (self):
+        return self._name 
+     
+restaurant = Restaurant("Dae Jang Geum")   
+print(restaurant.name())
---
+++ lib/review.py
@@ -0,0 +1,9 @@
+class Review:
+    def __init__ (self, customer, restaurant, rating):
+        self.customer = customer
+        self.restaurant = restaurant
+        self.rating = rating
+        
+    def rating(self):
+        return self.rating
+    
