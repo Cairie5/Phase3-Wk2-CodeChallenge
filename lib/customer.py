4d4930f9969d4c17af213a4c33598d94d74dec67
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-28 23:46:27 +0300

    updated customer

--- lib/customer.py
+++ lib/customer.py
@@ -10,6 +10,7 @@ class Customer:
         self.family_name = family_name  # Convert family_name to string
         self.full_name = None  # Initialize full_name to None
         self.set_full_name()   # Call the set_full_name method to set the full_name
+        self.reviews = []      # List to store associated reviews
         Customer.all_customers.append(self)  # Add the instance to the list of all customers
         
     # Set the given_name attribute
@@ -35,6 +36,58 @@ class Customer:
     def all(cls):
         return cls.all_customers
 
+    def restaurants(self):
+        # Returns a unique list of all restaurants this customer has reviewed
+        reviewed_restaurants = set()
+        for review in self.reviews:
+            reviewed_restaurants.add(review.restaurant)
+        return list(reviewed_restaurants)
+
+    def add_review(self, restaurant, rating):
+        # Creates a new review and associates it with this customer and restaurant
+        new_review = Review(self, restaurant, rating)
+        self.reviews.append(new_review)
+
+# Adding the Review class from the previous code
+class Review:
+    all_reviews = []
+
+    def __init__(self, customer, restaurant, rating=None):
+        self._customer = customer
+        self._restaurant = restaurant
+        self._rating = None
+        Review.all_reviews.append(self)
+        if rating is not None:
+            self.set_rating(rating)
+
+    def get_rating(self):
+        return self._rating
+
+    def set_rating(self, rating):
+        if isinstance(rating, int):
+            self._rating = rating
+        else:
+            print("Rating must be an integer")
+
+    rating = property(get_rating, set_rating)
+
+    def customer(self):
+        return self._customer
+
+    def restaurant(self):
+        return self._restaurant
+
+    @property
+    def restaurant_name(self):
+        return self._restaurant
+
+    @classmethod
+    def all(cls):
+        return [
+            f"Customer: {review.customer()}, Restaurant: {review.restaurant_name}, Rating: {review.rating}"
+            for review in cls.all_reviews
+        ]
+
 # Creating instances of the Customer class
 mycustomer = Customer("George", "Washington")
 newCustomer = Customer("Henry", "Kennedy")
@@ -43,3 +96,16 @@ newCustomer = Customer("Henry", "Kennedy")
 all_customers = Customer.all()
 for customer in all_customers:
     print(customer.full_name)
+
+# Adding reviews and accessing reviewed restaurants for a customer
+restaurant1 = "Dae Jang Geum"
+restaurant2 = "Pizza Palace"
+
+mycustomer.add_review(restaurant1, 5)
+mycustomer.add_review(restaurant2, 4)
+
+print("Reviewed Restaurants:", mycustomer.restaurants())
+
+# Getting all the reviews
+all_reviews = Review.all()
+print(all_reviews)
--- lib/restaurant.py
+++ lib/restaurant.py
@@ -6,7 +6,8 @@ class Restaurant:
         self._name = None
         # If a name is provided during initialization, call set_name method
         if name is not None:
-            self.set_name(name)  
+            self.set_name(name)
+        self.reviews = []  # List to store associated reviews
         
     def set_name(self, name):
         # Check if the input name is a string
@@ -16,7 +17,18 @@ class Restaurant:
     
     def get_name(self):
         # Return the value of the private attribute _name
-        return self._name  
+        return self._name
+
+    def reviews(self):
+        # Return a list of all reviews for this restaurant
+        return [review for review in self.reviews]
+
+    def customers(self):
+        # Return a unique list of all customers who have reviewed this restaurant
+        reviewed_customers = set()
+        for review in self.reviews:
+            reviewed_customers.add(review.customer())
+        return list(reviewed_customers)
 
 # Create an instance of the Restaurant class with the name "Dae Jang Geum"
 restaurant = Restaurant("Dae Jang Geum")
