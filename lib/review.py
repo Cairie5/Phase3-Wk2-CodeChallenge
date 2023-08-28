58bd892cb18cf9cba0b27ce6820d01ae9bea1312
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-29 00:04:22 +0300

    changes

--- lib/review.py
+++ lib/review.py
@@ -47,12 +47,5 @@ class Review:
             for review in cls.all_reviews
         ]
 
-# Creating an instance of the review class
-myreview = Review("Patience", "Dae Jang Geum", 10)
-
-# Getting the rating of the review using the rating property
-print(myreview.rating)
-
-# Getting all the reviews
-all_reviews = Review.all()
-print(all_reviews)
+# Rest of the code (Restaurant and Customer classes) remains unchanged
+# ... (Paste your Restaurant and Customer class code here)
